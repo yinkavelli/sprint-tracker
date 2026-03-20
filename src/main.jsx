b03765e5f60488375df386fe00dbad26fde1bfdf
yinkavelli <yinkavelli@gmail.com>
2026-03-20 14:14:50 +0400

    initial

---
+++ src/main.jsx
@@ -0,0 +1,4 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import SprintTracker from './SprintTracker.jsx'
+ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><SprintTracker /></React.StrictMode>)
